0c440d2c24a963406344a64ed24c6de2fab618e8
Shariif-Mohsoo <mr8781256@gmail.com>
2025-08-28 16:25:09 +0500

    STL vector

--- DSA_fromZero/STL/vector.cpp
+++ DSA_fromZero/STL/vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,18 +26,48 @@ int main()
     a.push_back(6);  // 2, 4, 6
     a.push_back(8);  // 2, 4, 6, 8
     a.push_back(10); // 2, 4, 6, 8, 10
-
+    a.push_back(1);
+    a.push_back(2);
     // remove element
     // a.pop_back();           // to remove last value
     // a.erase(a.begin() + 2); // to erase specific index value
     // a.clear();              // to clear/remove all elements.
 
-    cout << a.at(1) << endl;
+    cout
+        << a.at(1) << endl;
 
     // Iterator in vector
     for (auto it = a.begin(); it != a.end(); it++)
     {
         cout << *it << endl;
     }
+    cout << endl;
+    //
+    for (int i = 0; i < a.size(); i++)
+        cout << a[i] << endl;
+
+    // copy value of one vector to another
+    vector<int> v;
+    v = a;
+    cout << "Size of v:" << v.size() << endl;
+
+    //
+    for (auto i : a)
+        cout << i << " ";
+    // sort in increasing order
+    sort(a.begin(), a.end());
+
+    // sort in decreasing order
+    sort(a.begin(), a.end(), greater<int>());
+
+    for (auto i : a)
+        cout
+            << i << endl;
+
+    // search in vector
+    cout << binary_search(a.begin(), a.end(), 9) << endl;
+
+    // find index of
+    cout << (find(a.begin(), a.end(), 2) - a.begin()) << endl;
     return 0;
 }
